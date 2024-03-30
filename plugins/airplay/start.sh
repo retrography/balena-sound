95776fbc65a7e6e906b6530957bece51d3ceefe6
Mahmood S. Zargar <mahmood@gmail.com>
2024-03-30 16:23:31 +0100

    minor fix

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -18,7 +18,7 @@ exec shairport-sync \
 	--name "$SOUND_DEVICE_NAME" \
 	--output alsa \
 	-- -d "hw:usb-soundcard-1" \
-  | echo "Shairport-sync started. Device is discoverable as $SOUND_DEVICE_NAME"
+	-c "MICROMEGA USB Audio 2.0 Output Playback Vol" |
+	echo "Shairport-sync started. Device is discoverable as $SOUND_DEVICE_NAME"
 
-#	-c "MICROMEGA USB Audio 2.0 Output Playback Vol" \
-#	-i 1 \ 
+#	-i 1 \
